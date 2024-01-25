550d2197f29f5d61d2fa5986393e2716c525268f
jena--boot <uyenmai.811@gmail.com>
2024-01-25 14:30:31 +0700

    bai2.js

---
+++ bai2/bai2.js
@@ -0,0 +1,5 @@
+// alert: thanh thông báo chạy trên 1 trình duyệt web,
+// console.log: hàm peint của javascript : được hỗ trợ sẵn không cần định nghĩa hàm 
+// alert('Hello every body it is mineuyn')
+console.log('Hello everybody this is mineuyn')
+// có thể nhìn thấy output trong tag consoler, mục infor của web.
