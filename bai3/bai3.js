019f52592cc9f4693dd92ca87d6572b026b22bff
jena--boot <uyenmai.811@gmail.com>
2024-01-25 14:37:36 +0700

    bai3

---
+++ bai3/bai3.js
@@ -0,0 +1,19 @@
+// console.log('Hello')
+// khai báo biến trong js 
+// commen key word khai báo biến : let + kiểu dữ liệu
+let c = 5; // interger
+let d = 5.0; //float
+//  đối với string có thể dùng dấu " `` " - dấu backtick 
+//  đối với js thì string thuộc dạng text => dùng dấu backtick có thể sử dụng cả TH 2 dấu trong cùng 1 câu
+// VD: console.log(`She say you can call she is "Jenny" `)
+
+// keyword: typeof dùng để kiểm ra kiễu data của biến
+
+// hằng số có key world là const - tức là không thể thay thế nó bàng số khác
+// giống VD bên dưới web sẽ báo lỗi
+// vd: const x=5;
+// x=6; 
+// console.log(x)
+let x = 5, y = 6, z = 7;
+console.log("Test string", "x=", x, "y=", y, "z=", z)
+// NaN là 1 giá trị mà js trả ra khi biến cần in ko phải giá trị hay số
